909a9108abed1d76938714eaa1d12b104fda4001
Cepebe <cpedrazaber@gmail.com>
2024-03-11 00:13:34 +0100

    Calculation of the perimeter of the rectangle

--- rectangle.cpp
+++ rectangle.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main(){
     float side1, side2;
+    float perimeter;
+
     cout << "Welcome to the rectangle world" << endl;
     cout << "Please introduce the two sides of the rectangle" << endl;
     cin >> side1 >> side2;
+
+    perimeter = 2*(side1+side2);
+
+    cout << "The perimeter of the rectangle is" << perimeter ;
+
 }
