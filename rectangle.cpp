2c3c457a318979d5e38dbc9d5df8a55ba8e07b3e
Cepebe <cpedrazaber@gmail.com>
2024-03-10 23:57:28 +0100

    New branch with rectangle.cpp code

---
+++ rectangle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float side1, side2;
+    cout << "Welcome to the rectangle world" << endl;
+    cout << "Please introduce the two sides of the rectangle" << endl;
+    cin >> side1 >> side2;
+}
