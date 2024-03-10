102759f73ce875c5b4d37c9398e337b7b181c487
Cepebe <cpedrazaber@gmail.com>
2024-03-11 00:05:02 +0100

    Computation of the area of the rectangle

--- rectangle.cpp
+++ rectangle.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 int main(){
     float side1, side2;
+
+    float area;
+
     cout << "Welcome to the rectangle world" << endl;
     cout << "Please introduce the two sides of the rectangle" << endl;
     cin >> side1 >> side2;
+
+    area = side1*side2;
+
+    cout << "The area is " << area << endl;
+
+
 }
